22a49ad506983c97c885079bf523381ec7137198
randleem <erandle2211@gmail.com>
2022-10-31 13:41:20 +0000

    intro to types complete

--- typescript-course/app.js
+++ typescript-course/app.js
@@ -1,1 +1,14 @@
-console.log("Your code goes here...");
+function add(n1, n2, print, phrase) {
+    var result = n1 + n2;
+    if (print) {
+        console.log(phrase + result);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var num1 = 5;
+var num2 = 2.9;
+var printResult = true;
+var resultPhrase = "this is the number: ";
+add(num1, num2, printResult, resultPhrase);
--- typescript-course/app.ts
+++ typescript-course/app.ts
@@ -1,1 +1,14 @@
-console.log('Your code goes here...');
+function add(n1: number, n2: number, print: boolean, phrase: string) {
+  let result = n1 + n2;
+  if (print) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+}
+const num1 = 5;
+const num2 = 2.9;
+const printResult = true;
+const resultPhrase = "this is the number: ";
+
+add(num1, num2, printResult, resultPhrase);
